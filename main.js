55b81bccdf19f0ec7e1657b5c25ef17421388b84
sulit.bat.dahh <sulit.bat.dahh@gmail.com>
2024-06-04 12:32:04 +0000

    updated on 04/06/24

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-onload = () =>{
+onload = () => {
         document.body.classList.remove("container");
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+        const textElement = document.getElementById("text-section");
+        setTimeout(() => {
+                textElement.classList.remove("hidden")
+                textElement.classList.add("text-section-animation")
+        }, 6000);
+});
