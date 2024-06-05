25304327a45ae4e786864c52049710d8a6a96a8e
sulit.bat.dahh <sulit.bat.dahh@gmail.com>
2024-06-05 05:51:19 +0000

    updated on 05/06/24

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 onload = () => {
-        document.body.classList.remove("container");
-};
+        var getBersiap = document.getElementById('text-section-bersiap')
+        getBersiap.classList.remove("hidden")
+        getBersiap.classList.add("text-section-animation")
 
-document.addEventListener("DOMContentLoaded", function () {
-        const textElement = document.getElementById("text-section");
         setTimeout(() => {
-                textElement.classList.remove("hidden")
-                textElement.classList.add("text-section-animation")
-        }, 6000);
-});
+                getBersiap.classList.add("text-section-animation-bersiap")
+                document.getElementById('flower').classList.remove("container");
+                setTimeout(() => {
+                        var textElement = document.getElementById("text-section");
+                        textElement.classList.remove("hidden")
+                        textElement.classList.add("text-section-animation")
+                }, 6000);
+        }, 3000)
+};
